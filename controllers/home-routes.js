7403fce60ee72ca8a92c22735abfd8f916d4dd9b
TJ James <jamestw13@gmail.com>
2021-11-30 14:18:25 -0600

    small view changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -21,9 +21,11 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({plain: true}));
+      console.log(posts);
       res.render('homepage', {
         posts,
         loggedIn: req.session.loggedIn,
+        username: req.session.username,
       });
     })
     .catch(err => {
