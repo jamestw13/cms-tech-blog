ca04655e608f43412e004518a8be75e3a37746f7
TJ James <jamestw13@gmail.com>
2021-11-30 00:07:00 -0600

    added routes and dependencies(auth, etc.)

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
 
-const {Post, User, Comment} = require('../model');
+const {Post, User, Comment} = require('../models');
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
